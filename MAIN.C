82c5823653ee3d22cdbf5ce836beb0e0bf2ccaa2
Vishnuprasad C <94431536+Vishnuprasadc369@users.noreply.github.com>
2024-12-17 11:25:02 +0530

    Update MAIN.C

--- MAIN.C
+++ MAIN.C
@@ -10,14 +10,18 @@ uint8_t intr;//=0;
 
 HAL_UART_Receive_IT(&huart1, letter, 1);
 
-/* USER CODE END 2 */
 
-WHILE(1){
+
+/* USER CODE END 2 */
+ /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while(1){
 	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
 	HAL_Delay(100);
  	HAL_UART_Transmit(&huart1, buffer, strlen(buffer), HAL_MAX_DELAY);
 	memset(buffer, NULL , sizeof(buffer));
 	intr=0;
+/* USER CODE END WHILE */
 
 }
 
